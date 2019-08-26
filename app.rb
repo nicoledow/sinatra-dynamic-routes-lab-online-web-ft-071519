36ebdc5488a7fdbbf542c121e081f55bd930276d
Nicole Dow <nikki.dow71@gmail.com>
2019-08-26 21:20:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,6 +29,13 @@ class App < Sinatra::Base
   
   get '/:operation/:number1/:number2' do 
     @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    
+    case @operation
+    when "+"
+      ""
+  end
   end
   
 
